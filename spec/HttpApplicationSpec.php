43469a04a2758cdfd045b7f2d2c4e4c97b031da2
Vsevolods Mihailovs <cheelahim@gmail.com>
2016-12-07 12:21:19 +0200

    Move HttpApplication to Http package.

---
+++ spec/HttpApplicationSpec.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace spec\Venta\Http;
+
+use PhpSpec\ObjectBehavior;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Venta\Contracts\Config\Config;
+use Venta\Contracts\Container\Container;
+use Venta\Contracts\Kernel\Kernel;
+use Venta\Contracts\Routing\Delegate;
+use Venta\Contracts\Routing\MiddlewarePipeline;
+use Venta\Contracts\Routing\MiddlewarePipelineFactory;
+use Venta\Contracts\Routing\Router;
+use Venta\Http\HttpApplication;
+
+class HttpApplicationSpec extends ObjectBehavior
+{
+
+    function it_is_initializable_and_boots_kernel(Kernel $kernel, Container $container)
+    {
+        $kernel->boot()->willReturn($container)->shouldBeCalled();
+        $this->beConstructedWith($kernel);
+        $this->shouldHaveType(HttpApplication::class);
+        $this->shouldImplement(Delegate::class);
+    }
+
+    function it_runs(
+        Kernel $kernel,
+        Container $container,
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        Config $config,
+        MiddlewarePipelineFactory $factory,
+        Router $router,
+        MiddlewarePipeline $pipeline
+    ) {
+        $kernel->boot()->willReturn($container);
+        $this->beConstructedWith($kernel);
+        $container->get(MiddlewarePipelineFactory::class)->willReturn($factory);
+        $factory->create([])->willReturn($pipeline);
+        $container->get(Config::class)->willReturn($config);
+        $config->get('middlewares', [])->willReturnArgument(1);
+        $container->get(Router::class)->willReturn($router);
+        $pipeline->process($request, $router)->willReturn($response);
+
+        $this->run($request)->shouldBe($response);
+        $this->next($request)->shouldBe($response);
+    }
+}
---
+++ src/HttpApplication.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types = 1);
+
+namespace Venta\Http;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Venta\Contracts\Config\Config;
+use Venta\Contracts\Container\Container;
+use Venta\Contracts\Kernel\Kernel;
+use Venta\Contracts\Routing\Delegate;
+use Venta\Contracts\Routing\MiddlewarePipelineFactory;
+use Venta\Contracts\Routing\Router;
+
+
+/**
+ * Class HttpApplication
+ *
+ * @package Venta\Framework\Http
+ */
+final class HttpApplication implements Delegate
+{
+
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * HttpApplication constructor.
+     *
+     * @param Kernel $kernel
+     */
+    public function __construct(Kernel $kernel)
+    {
+        $this->container = $kernel->boot();
+    }
+
+    /**
+     * Returns service container instance.
+     *
+     * @return Container
+     */
+    public function getContainer(): Container
+    {
+        return $this->container;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function next(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->run($request);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(ServerRequestInterface $request)
+    {
+        /** @var MiddlewarePipelineFactory $factory */
+        $factory = $this->container->get(MiddlewarePipelineFactory::class);
+        $pipeline = $factory->create($this->container->get(Config::class)->get('middlewares', []));
+        /** @var Router $router */
+        $router = $this->container->get(Router::class);
+
+        return $pipeline->process($request, $router);
+    }
+
+}
